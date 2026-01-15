1da0e70d6716a0b76a03480a13dfcf902967ff30
Kashish <kashishvishnoi821@gmail.com>
2026-01-15 18:55:13 +0530

    basic problem

---
+++ Basic-Program/ConvertKilometersToMiles.java
@@ -0,0 +1,7 @@
+public class ConvertKilometersToMiles{
+	public static void main(String[] args){
+	double Kilometer = 10.8;
+	double Miles = Kilometer * 1.6;
+	System.out.println("The distance"+Kilometer+ " km in miles is "+ Miles);
+	}
+}
