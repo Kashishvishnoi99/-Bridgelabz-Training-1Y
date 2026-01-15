ebabced2c4caf34f5b2223211d4cb79151c14902
Kashish <kashishvishnoi821@gmail.com>
2026-01-15 21:14:05 +0530

    basic problem

---
+++ Basic-Program/ProblemNo5.java
@@ -0,0 +1,11 @@
+public class ProblemNo5{
+public static void main(String[] args){
+	
+int TotalPens = 14;
+int Student = 3;
+int PenPerStudent = TotalPens / Student;
+int RemainngPen = TotalPens % Student;
+
+System.out.println("The Pen Per Student is "+ PenPerStudent +" and the remainng Pen not distributed is "+ RemainngPen); 
+}
+}
