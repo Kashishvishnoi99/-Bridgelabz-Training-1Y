d359089b7362977649dbf4dcd890a19e98bb757b
Kashish <kashishvishnoi821@gmail.com>
2026-01-15 18:26:42 +0530

    basic problem

---
+++ Basic-Program/AveragePercent.java
@@ -0,0 +1,10 @@
+public class AveragePercent{
+	public static void main(String[] args){
+    String name = "Sam's";
+    int Physics = 95;
+	int Chemistry = 96;
+	int Maths = 94;
+	int Average = (Physics + Chemistry +Maths)/3*100;
+	System.out.println(name +" average mark in PCM is "+Average);
+	}
+}
