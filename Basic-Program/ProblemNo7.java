38176fad1a8e8cbd05612070b5d2ca303d177fb5
Kashish <kashishvishnoi821@gmail.com>
2026-01-15 21:38:09 +0530

    basic problem

--- Basic-Program/ProblemNo7.java
+++ Basic-Program/ProblemNo7.java
@@ -3,8 +3,7 @@ public static void main(String[] args){
 	
 	double radius = 6378;
 	double VolumeSphereInKm = 4/3 * 3.14 * radius * radius * radius;
-	// 1km = 1.6 miles;
-	double VolumeSphereInMiles = 4/3 * 3.14 * radius * radius * radius * 1.6;
+	double VolumeSphereInMiles =  VolumeSphereInKm * 0.239913;
 	
 	System.out.println("The Volume of Earth in cubic kilometers is "+  VolumeSphereInKm + " and cubic miles is " + VolumeSphereInMiles);
 }
